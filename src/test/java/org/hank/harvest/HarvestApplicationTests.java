413b624e569d7e7aa09cc8abd8b25cf6d1651c11
Hank Wu <MS.hankwu@outlook.com>
2016-04-13 18:34:50 +0800

    feat(Harvest): apply spring-boot framework core

---
+++ src/test/java/org/hank/harvest/HarvestApplicationTests.java
@@ -0,0 +1,18 @@
+package org.hank.harvest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = HarvestApplication.class)
+@WebAppConfiguration
+public class HarvestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
