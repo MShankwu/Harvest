d02f606ee5d3288ae5f7f8c1eb71c8e8fc69416e
Hank Wu <MS.hankwu@outlook.com>
2016-04-21 16:48:48 +0800

    refactor(controller): rename IndexController->PageController

--- src/main/java/org/hank/harvest/controller/PageController.java
+++ src/main/java/org/hank/harvest/controller/PageController.java
@@ -7,11 +7,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * Created by Administrator on 2016/4/17.
  */
 @Controller
-public class IndexController {
+public class PageController {
 
     @RequestMapping("/")
     public String showIndex() {
         return "index";
+    }
+
+    @RequestMapping("/register")
+    public String showRegister() {
+        return "register";
     }
 
 }
