30532e70ca0dd1b45ddf9749bd91dd85c907d495
Hank Wu <MS.hankwu@outlook.com>
2016-04-23 17:10:55 +0800

    feat(controller): add /register.do action

--- src/main/java/org/hank/harvest/action/PageController.java
+++ src/main/java/org/hank/harvest/action/PageController.java
@@ -2,6 +2,7 @@ package org.hank.harvest.action;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by Administrator on 2016/4/17.
@@ -9,12 +10,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class PageController {
 
-    @RequestMapping("/")
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String showIndex() {
         return "index";
     }
 
-    @RequestMapping("/register")
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
     public String showRegister() {
         return "register";
     }
